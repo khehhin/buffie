2181e9f108b5d863275e04da1d1b82521b81da38
Gerald Lim <khehhin@yahoo.com>
2020-08-22 19:01:04 +0800

    Managed to get menu names

--- functions/index.js
+++ functions/index.js
@@ -8,65 +8,62 @@ const {Card, Suggestion} = require('dialogflow-fulfillment');
 const {Payload} = require('dialogflow-fulfillment');
 const http = require('http');
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
- 
+
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-  const agent = new WebhookClient({ request, response });
-  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
-  var menuTypes = [];
-  var occassionType = "";
-  var numPax = 0;
+    const agent = new WebhookClient({ request, response });
+    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+    var menuTypes = [];
+    var occassionType = "";
+    var numPax = 0;
 
-  function welcome(agent) {
-      // agent.add("Hi! I'm Buffy, the Neo Garden Bot!!");
-      // agent.add("What can I do for you today?");
-      // agent.add(new Suggestion(`Recommend a buffet menu`));
-      // agent.add(new Suggestion(`Some other requests`));
+    function welcome(agent) {
+        // Get List of Cuisines (MenuTypes)
+        let options = {
+            host: "40.119.212.144",
+            path: "/beta_delihub/api/Menu/GetMenuTypes",
+            method: 'GET',
+            // authentication headers
+            headers: {
+                'Authorization': 'Basic ' + new Buffer.from('DH_Xruptive' + ':' + 'DH_XPT@2020').toString('base64'),
+                'Content-Type': 'application/json'
+            }
+        };
 
-      // Get List of Cuisines (MenuTypes)
-      let options = {
-          host: "40.119.212.144",
-          path: "/beta_delihub/api/Menu/GetMenuTypes",
-          method: 'GET',
-          // authentication headers
-          headers: {
-              'Authorization': 'Basic ' + new Buffer.from('DH_Xruptive' + ':' + 'DH_XPT@2020').toString('base64'),
-              'Content-Type': 'application/json'
-          }
-      };
+        return new Promise(function(resolve, reject) {
+            // Do async job
+            var req = http.request(options, function (response) {
+                var str = '';
+                response.on('error', function (err) {
+                    reject(err);
+                });
+                response.on('data', function (chunk) {
+                    str += chunk;
+                });
+                response.on('end', function () {
+                    console.log("Ended, result is : " + str);
+                    resolve(JSON.parse(str));
+                });
+            });
+            req.end();
+        }).then(function (result) {
+            let count = result.length;
+            menuTypes = result;
+            agent.add(count + " menu types found.");
+            agent.add(menuTypes[0].MenuTypeID + " is " + menuTypes[0].MenuTypeName)
 
-      return new Promise(function(resolve, reject) {
-          // Do async job
-          var req = http.request(options, function (response) {
-              var str = '';
-              response.on('error', function (err) {
-                  reject(err);
-              });
-              response.on('data', function (chunk) {
-                  str += chunk;
-              });
-              response.on('end', function () {
-                  console.log("Ended, result is : " + str);
-                  resolve(JSON.parse(str));
-              });
-          });
-          req.end();
-      }).then(function (result) {
-          let count = result.length;
-          agent.add(count + " menu types found.");
+            agent.add("Hi! I'm Buffy, the Neo Garden Bot!!");
+            agent.add("What can I do for you today?");
+            agent.add(new Suggestion(`Recommend a buffet menu`));
+            agent.add(new Suggestion(`Some other requests`));
 
-          agent.add("Hi! I'm Buffy, the Neo Garden Bot!!");
-          agent.add("What can I do for you today?");
-          agent.add(new Suggestion(`Recommend a buffet menu`));
-          agent.add(new Suggestion(`Some other requests`));
+        });
+    }
 
-      });
-  }
-
-  function fallback(agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
-  }
+    function fallback(agent) {
+        agent.add(`I didn't understand`);
+        agent.add(`I'm sorry, can you try again?`);
+    }
 
     function occasion(agent) {
         agent.add(`Sure! What occasion are you planning the buffet for?`);
@@ -108,53 +105,53 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     }
 
     function pax(agent) {
-      var occassionType = agent.parameters.occassionType;
+        var occassionType = agent.parameters.occassionType;
 
-      return new Promise(function(resolve, reject) {
-          // Do async job
-          var req = http.request(options, function (response) {
-              var str = '';
-              response.on('error', function (err) {
-                  reject(err);
-              });
-              response.on('data', function (chunk) {
-                  str += chunk;
-              });
-              response.on('end', function () {
-                  console.log("Ended, result is : " + str);
-                  resolve(JSON.parse(str));
-              });
-          });
-          req.write(JSON.stringify(bodyData));
-          req.end();
-      }).then(function (result) {
-          let count = result.length;
-          agent.add(`Ok, you are expecting ${numPax} pax for your ${occassionType}`);
-          agent.add(`Any dietary preference?`);
-          agent.add(new Suggestion(`Vegetarian only`));
-          agent.add(new Suggestion(`Halal`));
-          agent.add(new Suggestion(`Vegetarian and Halal`));
-          agent.add(new Suggestion(`No restriction`));
+        return new Promise(function(resolve, reject) {
+            // Do async job
+            var req = http.request(options, function (response) {
+                var str = '';
+                response.on('error', function (err) {
+                    reject(err);
+                });
+                response.on('data', function (chunk) {
+                    str += chunk;
+                });
+                response.on('end', function () {
+                    console.log("Ended, result is : " + str);
+                    resolve(JSON.parse(str));
+                });
+            });
+            req.write(JSON.stringify(bodyData));
+            req.end();
+        }).then(function (result) {
+            let count = result.length;
+            agent.add(`Ok, you are expecting ${numPax} pax for your ${occassionType}`);
+            agent.add(`Any dietary preference?`);
+            agent.add(new Suggestion(`Vegetarian only`));
+            agent.add(new Suggestion(`Halal`));
+            agent.add(new Suggestion(`Vegetarian and Halal`));
+            agent.add(new Suggestion(`No restriction`));
 
-          // agent.add(count + " objects found in menu array.");
-          // agent.add(`Here's a recommendation for your ${occassionType} `);
+            // agent.add(count + " objects found in menu array.");
+            // agent.add(`Here's a recommendation for your ${occassionType} `);
 
-          // agent.add(new Card({
-          //       title: result[1].MenuNameE_Online,
-          //       imageUrl: "https://www.gstatic.com/webp/gallery/4.jpg",
-          //         text: "This is some example text...",
-          //         buttonText: "Button",
-          //         buttonUrl: "https://www.rp.edu.sg"
-          //     })
-          // );
-          // agent.add(new Suggestion(`Quick Reply`));
-          // agent.add(new Suggestion(`Suggestion`));
+            // agent.add(new Card({
+            //       title: result[1].MenuNameE_Online,
+            //       imageUrl: "https://www.gstatic.com/webp/gallery/4.jpg",
+            //         text: "This is some example text...",
+            //         buttonText: "Button",
+            //         buttonUrl: "https://www.rp.edu.sg"
+            //     })
+            // );
+            // agent.add(new Suggestion(`Quick Reply`));
+            // agent.add(new Suggestion(`Suggestion`));
 
-          // agent.add(new Payload("PLATFORM_UNSPECIFIED", payLoad));
-          // agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
-      });
+            // agent.add(new Payload("PLATFORM_UNSPECIFIED", payLoad));
+            // agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+        });
 
-  }
+    }
 
 
     function cuisine(agent) {
@@ -361,8 +358,8 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
             // agent.add(`Here's a recommendation for your ${occassionType} `);
 
             agent.add(new Card({
-                  title: result[1].MenuNameE_Online,
-                  imageUrl: "https://www.gstatic.com/webp/gallery/4.jpg",
+                    title: result[1].MenuNameE_Online,
+                    imageUrl: "https://www.gstatic.com/webp/gallery/4.jpg",
                     text: "This is some example text...",
                     buttonText: "Button",
                     buttonUrl: "https://www.rp.edu.sg"
@@ -380,18 +377,18 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 
 
 
-  // // Uncomment and edit to make your own Google Assistant intent handler
-  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function googleAssistantHandler(agent) {
-  //   let conv = agent.conv(); // Get Actions on Google library conv instance
-  //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
-  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
-  // }
-  // // See https://github.com/dialogflow/fulfillment-actions-library-nodejs
-  // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
+    // // Uncomment and edit to make your own Google Assistant intent handler
+    // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
+    // // below to get this function to be run when a Dialogflow intent is matched
+    // function googleAssistantHandler(agent) {
+    //   let conv = agent.conv(); // Get Actions on Google library conv instance
+    //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
+    //   agent.add(conv); // Add Actions on Google library responses to your agent's response
+    // }
+    // // See https://github.com/dialogflow/fulfillment-actions-library-nodejs
+    // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
 
-  // Run the proper function handler based on the matched Dialogflow intent name
+    // Run the proper function handler based on the matched Dialogflow intent name
     let intentMap = new Map();
     intentMap.set('Default Welcome Intent', welcome);
     intentMap.set('Default Fallback Intent', fallback);
@@ -401,5 +398,5 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     intentMap.set('Cuisine', cuisine);
 
     // intentMap.set('your intent name here', googleAssistantHandler);
-  agent.handleRequest(intentMap);
+    agent.handleRequest(intentMap);
 });
